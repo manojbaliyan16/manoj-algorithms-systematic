0edbad8c4b30bd15f18ce5274d675cc8cae240ba
Manoj <70482944+manojbaliyan16@users.noreply.github.com>
2026-04-27 09:37:54 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.cpp
@@ -0,0 +1,31 @@
+
+class Solution {
+    bool helper(string &s, int l, int r, bool deleted)
+    {
+        while(l<=r)
+        {
+            
+            if((s[l])!=(s[r]))
+            {
+               if(deleted)
+                {
+                    return false; // already used once chance
+                }
+                return helper(s, l+1, r, true) || helper(s, l, r-1, true);     
+
+                }
+                l++;
+                r--;
+        }
+        return true;
+    }
+public:
+    
+    bool validPalindrome(string s) {
+     if(s.length()==0 || s.length()==1)
+    {
+        return true;
+    }
+    return helper(s, 0, s.length()-1, false);
+    }
+};
